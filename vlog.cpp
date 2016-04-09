e575f119367157a371d887fb324decb8612f0dce
Dimitriy Yun <pantigalt@gmail.com>
2016-04-09 01:18:43 -0400

    added new library

--- vlog.cpp
+++ vlog.cpp
@@ -1,6 +1,14 @@
 #include "vlog.h"
+#include <string>
+#include <iostream>
 
+using namespace std;
 
 vlog::vlog()
 {
+}
+
+void vlog::write(const string &message)
+{
+    cout << message << endl;
 }
--- vlog.h
+++ vlog.h
@@ -1,12 +1,13 @@
 #ifndef VLOG_H
 #define VLOG_H
-
+#include <string>
 
 class vlog
 {
 
 public:
     vlog();
+    void write(const std::string& message);
 };
 
 #endif // VLOG_H
